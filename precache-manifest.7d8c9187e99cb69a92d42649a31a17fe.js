7af813b5326c5b10821435fe93389448132dee86
Mariusz Mańka <mariuszmanka2@gmail.com>
2019-04-19 13:39:15 +0200

    Updates

---
+++ precache-manifest.7d8c9187e99cb69a92d42649a31a17fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  },
+  {
+    "revision": "04201e5d199506a37aaa",
+    "url": "/react-weather-app/static/js/main.b160945d.chunk.js"
+  },
+  {
+    "revision": "b5e40cfdb402e74d07f6",
+    "url": "/react-weather-app/static/js/2.dfc31265.chunk.js"
+  },
+  {
+    "revision": "04201e5d199506a37aaa",
+    "url": "/react-weather-app/static/css/main.127536ea.chunk.css"
+  },
+  {
+    "revision": "4bfd7307c74c0c3315a7f7346142f3aa",
+    "url": "/react-weather-app/index.html"
+  }
+];
