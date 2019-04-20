9bfd838047110cc9ec5c62f8d4f84cb3e084ab9a
Mariusz Mańka <mariuszmanka2@gmail.com>
2019-04-20 12:01:19 +0200

    Updates

--- precache-manifest.cb2d9803e21a884d0fa700f58253f3a8.js
+++ precache-manifest.cb2d9803e21a884d0fa700f58253f3a8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
   },
   {
-    "revision": "f89a9d41275c0f37e1d1",
-    "url": "/react-weather-app/static/js/main.08c89de5.chunk.js"
+    "revision": "36674c79c2b5f5d19a36",
+    "url": "/react-weather-app/static/js/main.cb0e9507.chunk.js"
   },
   {
     "revision": "8cf9e9e2ccf7c51ade07",
     "url": "/react-weather-app/static/js/2.1acef893.chunk.js"
   },
   {
-    "revision": "f89a9d41275c0f37e1d1",
+    "revision": "36674c79c2b5f5d19a36",
     "url": "/react-weather-app/static/css/main.640a8264.chunk.css"
   },
   {
-    "revision": "de03d3740dfda49279e4452479dfb629",
+    "revision": "478b368e1483c4c450dbb94a6ec7187b",
     "url": "/react-weather-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-weather-app/precache-manifest.0d67a1448829ae947134730ed68fdea7.js"
+  "/react-weather-app/precache-manifest.cb2d9803e21a884d0fa700f58253f3a8.js"
 );
 
 workbox.clientsClaim();
